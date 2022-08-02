36888db545327768088476736cad771869b70ed2
Mubarek Seid <mubareksd@gmail.com>
2022-08-03 00:10:09 +0300

    print

---
+++ print.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print - print char.
+ * @str: string.
+ *
+ * Return: string length.
+**/
+
+int print(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+		_putchar(str[i]);
+
+	return (i);
+}
