c60cde51a06977de30670d655eb61c0c30716897
Mubarek Seid <mubareksd@gmail.com>
2022-08-03 00:23:18 +0300

    redo

--- main.h
+++ main.h
@@ -26,6 +26,7 @@ typedef struct _formatx
 
 int _strlen(const char *);
 int print(char *);
+char *itoa(long int, int);
 
 /* printf.c */
 int _printf(const char *, ...);
@@ -36,6 +37,15 @@ int percent_handler(const char *, va_list, int *);
 /* printers */
 int print_string(va_list);
 int print_char(va_list);
+int print_integer(va_list);
+int print_binary(va_list);
+int print_rot(va_list);
+int print_unsigned(va_list);
+int print_octal(va_list);
+int print_hexadecimal_low(va_list);
+int print_hexadecimal_upp(va_list);
+int print_pointer(va_list);
+int print_rev_string(va_list);
 
 /* _putchar.c */
 int _putchar(char);
